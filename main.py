dc0d72bc860a672bc769d922179a57735e1be66b
ra-beat <ra-beat@yandex.ru>
2023-05-18 09:20:07 +0300

    main-proc

---
+++ main.py
@@ -0,0 +1,75 @@
+#pip install torchvision==0.10.1
+#pip install torch==1.9.1
+from utils.general import scale_coords
+from images_storage import SaveImage
+import torch
+from Video_detector import VideoDetector
+from road_accident import RoadAccidentFinder
+
+
+def processing():
+
+    # обработка прогнозов, получение данных для обработки
+    for det, camera_id, image, img_resize in detector:
+        if camera_id is None:
+            continue
+        try:
+            det[:, :4] = scale_coords(img_resize, det[:, :4], image.shape).round()
+        except IndexError:
+            det = torch.empty(0, 6)
+
+        if not torch.any(det):  # ожидание новых данных
+            continue
+
+        # сохраняю тестовое изображение, только в режиме теста
+        if save_test_image:
+            save_image.show_detection_result(det, image, camera_id)
+
+        # поиск аварий
+        if (accident_object := road_accident_finder.road_accident_process(camera_id, det)) is not None:
+            print("  Find accident!!!  for objects:  " + str(accident_object[0]))
+            save_image.save_road_accident_image(image, accident_object[0])
+        if save_test_image:
+            save_image.save_test_image(camera_id, road_accident_finder.get_test_map(camera_id))
+
+
+if __name__ == '__main__':
+
+    save_test_image = True  #False
+
+    car_cls = 1
+
+    cuda_type = "cpu"
+    weights = "weights/yolov5l.pt"
+    image_size_input = 640
+    object_confidence_threshold = 0.5
+    IOU_threshold_NMS = 0.45
+    image_resolution = 1280
+    capture_interval = 10
+
+    # отправляю кадр, для детектора
+    detector = VideoDetector(
+        cuda_type, weights, image_size_input,
+        object_confidence_threshold, IOU_threshold_NMS,
+        image_resolution, capture_interval)
+
+    # сохраняю изображение с указанным размером
+    save_image = SaveImage("/home/user/simple_road_accident/", "road_accident", "test_image", detector.names, car_cls, detector.scale, 1280)
+
+    road_accident_finder = RoadAccidentFinder(car_cls=car_cls, min_static_time=80, frame_capture_interval=10, max_point_attenuation=2000)
+
+    connection_data = [
+            (3029, "https://s3.moidom-stream.ru/s/public/0000003029.m3u8"),
+            (6207, "https://s3.moidom-stream.ru/s/public/0000006207.m3u8")
+        ]
+
+    detector.set_cameras(connection_data)
+    road_accident_finder.set_camera_connection_list(connection_data)
+
+    # запуск главного процесса
+    while True:
+        processing()
+
+
+
+
